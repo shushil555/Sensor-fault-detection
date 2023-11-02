8385404f590b32ea3c54cdfce592633fc03323b9
Shushil Paudel <51052033+shushil555@users.noreply.github.com>
2023-11-02 19:18:09 +0545

    test

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements():
+    requirement_list = []
+    file_path = 'requirements.txt'
+    with open(file_path) as file:
+        requirement_list = file.readlines()
+        requirement_list = [require.replace('\n','') for require in requirement_list]
+        if '- e. ' in requirement_list:
+            requirement_list.remove('- e. ')
+    return requirement_list
+
+
+setup(
+    name='sensor',
+    version='1.0.1',
+    description='this is end to end machine learning project',
+    author='Rajan Devkota',
+    author_email='r.devkota.98@gmail.com',
+    packages=find_packages(),
+    install_require = get_requirements(),
+)
+
